1063d6fec3bffc4ccc0242a82bc1a0811fc0aaa1
chuck 2.0 <bozicslxye1@gmail.com>
2017-07-04 17:32:04 +0300

    The intial commit of the OOP class

--- oppcase.py
+++ oppcase.py
@@ -1,10 +1,89 @@
+class phone(object):
+    def __init__(self,size,phone_type,screen_attributes,os_type):
+        self.size=size
+        self.phone_type=phone_type
+        self.screen_attributes=screen_attributes
+        self.os_type=os_type
 
-class animal(object):
-    def __init__(self,type,num_legs,food_eaten,body_skin):
-        self.type=type
-        self.num_legs=num_legs
-        self.food_eaten=food_eaten
-        self.body_skin=body_skin
+    def call(self):
+        self.number=number
+        print('calling the number '+number)
+        return "calling the number !" +number
 
-    def can_:
-        
+    def recieve(self):
+        self.number=number
+        print('Recieving call from the number '+number)
+        return "Recieving call from the number " +number
+
+class smart_phone(phone):
+    def __init__(self,size,phone_type,screen_attributes,os_type):
+        self.size=size
+        self.phone_type=phone_type
+        self.screen_attributes=screen_attributes
+        self.os_type=os_type
+    
+        self.size=5.7 
+        self.phone_type='smart_phone'
+        self.os_type='android_os or Ios'
+        self.screen_attributes={'screen':'Touch_screen','multi-touch':True}
+
+    def can_browse(self):
+        self.web_page='www.twitter.com'
+        print('I am browsing the internet '+str(self.web_page))
+
+    def can_watch_and_stream(self):
+        self.videos='youtube videos playing on my device'
+        print('watching '+(self.videos))
+
+
+
+class smart_phone(phone):
+    def __init__(self,size,phone_type,screen_attributes,os_type):
+        autorotate=90
+        self.size=size
+        self.phone_type=phone_type
+        self.screen_attributes=screen_attributes
+        self.os_type=os_type
+        self.__autorotate=autorotate
+    
+        self.size=5.7 
+        self.phone_type='smart_phone'
+        self.os_type='android_os or Ios'
+        self.screen_attributes={'screen':'Touch_screen','multi-touch':True}
+
+    def can_browse(self):
+        self.web_page='www.twitter.com'
+        print('I am browsing the internet '+str(self.web_page))
+
+class non_smart_phone(phone):
+        def __init__(self,size,phone_type,screen_attributes,os_type):
+            keyboard = 'qwerty'
+            self.size=size
+            self.phone_type=phone_type
+            self.screen_attributes=screen_attributes
+            self.os_type=os_type
+            self.__keyboard=keyboard
+
+
+            self.size=2
+            self.phone_type='regular_phone'
+            self.os_type='taizen_os'
+            self.screen_attributes={'screen':'non_touch_screen','buttons':True,'Touch_screen':False}
+
+        def play_snake_game(self):
+            self.game='Preinstalled game of snake to be played by user'
+
+            print('I always play the '+self.game)
+
+        def keyboard(self):
+            self.press='i normally interact with the key board'
+
+            print(self.press)
+
+
+
+s=smart_phone(5.7,'smart_phone','android_os or Ios',{'screen':'Touch_screen','multi-touch':True} )
+s.can_browse()
+
+n=non_smart_phone(2,'regular_phone','taizen_os',{'screen':'non_touch_screen','buttons':True,'Touch_screen':False})
+n.play_snake_game()
