a72258bbf69e8a2f2c3c1d81e701e061c0ca8c85
chuck 2.0 <bozicslxye1@gmail.com>
2017-07-04 18:08:42 +0300

    did some test on one method

--- testcase.py
+++ testcase.py
@@ -1,12 +1,9 @@
 import unittest
+import oopcase
 
-class Mytest(unittest.test):
+class Mytest(unittest.TestCase):
     """ return the unit test of the methods defined in the oopcase class"""
 
-    def test_isMammal(self):
-        self.assertEqual(isCow('cow'),'True')
-
-
-
-
-unittest.main()
+    def test_call(self):
+        p=oopcase.phone()
+        self.assertEqual(p.call(123),'calling the number !123')
